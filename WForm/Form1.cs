0057430ab8fd250a29054d54879986b05edb2e1c
ohenrygithub <iamohenry@163.com>
2020-11-30 15:13:48 +0800

    C# Form call Qt gui dll project. C# part.

--- WForm/Form1.cs
+++ WForm/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace WForm
 {
     public partial class Form1 : Form
     {
+        [DllImport(@"qtdialog.dll")]
+        public static extern bool showDialog(IntPtr parent);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showDialog(this.Handle);
         }
     }
 }
